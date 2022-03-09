050b493592f6d2bc504a5500d0db8f4f5d1e89a0
ferreira-joao <klausblack231@gmail.com>
2022-03-09 14:19:44 -0300

    initial config

---
+++ app/src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import styles from "./styles";
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text>
+    </View>
+  );
+}
+
+export default Home;
